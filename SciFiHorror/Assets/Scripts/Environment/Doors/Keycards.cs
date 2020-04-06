9757d3a916eb548ea79ac4ff47040616e624db22
Harry <profawesum@gmail.com>
2020-04-07 11:41:55 +1200

    Working on getting keycards and doors integrated

---
+++ SciFiHorror/Assets/Scripts/Environment/Doors/Keycards.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Keycards
+{
+    //static bools for whether the player has the keycard or not
+    private static bool yellowKeyCard = false;
+    private static bool redKeyCard = false;
+    private static bool blueKeyCard = false;
+    private static bool greenKeyCard = false;
+
+
+    //checks to see if the player has the yellow key card
+    public static bool gotYellowCard
+    {
+        get
+        {
+            return yellowKeyCard;
+        }
+        set
+        {
+            yellowKeyCard = value;
+        }
+    }
+
+    //checks to see if the player has the red key card
+    public static bool gotRedCard
+    {
+        get
+        {
+            return redKeyCard;
+        }
+        set
+        {
+            redKeyCard = value;
+        }
+    }
+
+    //checks to see if the player has the blue key card.
+    public static bool gotBlueCard
+    {
+        get
+        {
+            return blueKeyCard;
+        }
+        set
+        {
+            blueKeyCard = value;
+        }
+    }
+
+    //checks to see if the player has the green keycard
+    public static bool gotGreenCard
+    {
+        get
+        {
+            return greenKeyCard;
+        }
+        set
+        {
+            greenKeyCard = value;
+        }
+    }
+
+
+}
---
+++ SciFiHorror/Assets/Scripts/Environment/Doors/OpenDoors.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoors : MonoBehaviour
+{
+
+    [Header("Does the door require a keycard?")]
+    //reference for which keycard the door requires
+    public bool yellowCard;
+    public bool redCard;
+    public bool blueCard;
+    public bool greenCard;
+    public bool noCard;
+
+    [Header("Reference to the animator")]
+    //reference to the animator
+    public Animator animator;
+
+    private void Start()
+    {
+        //find the animator
+        animator = this.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (yellowCard && Keycards.gotYellowCard) {
+            openTheDoor();
+        }
+        if (redCard && Keycards.gotRedCard)
+        {
+            openTheDoor();
+        }
+        if (blueCard && Keycards.gotBlueCard)
+        {
+            openTheDoor();
+        }
+        if (greenCard && Keycards.gotGreenCard)
+        {
+            openTheDoor();
+        }
+        if (noCard) {
+            openTheDoor();
+        }
+    }
+
+
+    //what to do if the door should open
+    public void openTheDoor() {
+        animator.SetBool("character_nearby", true);   
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        animator.SetBool("character_nearby", false);
+    }
+
+}
+
+
